438230f06f59009efe88652f89500c6a0e44c3c9
blf11139 <b8991999@gmail.com>
2019-03-26 17:39:27 -0400

    Update __init__.py

--- spacyjsonnlp/__init__.py
+++ spacyjsonnlp/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-(C) 2019 Damir Cavar, Oren Baldinger, Maanvitha Gongalla, Anurag Kumar, Murali Kammili
+(C) 2019 Damir Cavar, Oren Baldinger, Maanvitha Gongalla, Anurag Kumar, Murali Kammili, Boli Fang
 
 Wrappers for spaCy to JSON-NLP output format.
 
