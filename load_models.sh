ab1fa2ffcc93d4ed830f916957336f71b986b8f4
Damir Cavar <dcavar@me.com>
2019-04-23 17:29:00 -0400

    Simple changes in the caching function.

--- load_models.sh
+++ load_models.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 python3 -m spacy download en_core_web_sm
+python3 -m spacy download en
 python3 -m spacy download de_core_news_sm
+python3 -m spacy download de
 python3 -m spacy download fr_core_news_sm
+python3 -m spacy download fr
 python3 -m spacy download es_core_news_sm
+python3 -m spacy download es
 python3 -m spacy download pt_core_news_sm
+python3 -m spacy download pt
 python3 -m spacy download it_core_news_sm
+python3 -m spacy download it
 python3 -m spacy download nl_core_news_sm
+python3 -m spacy download nl
 python3 -m spacy download el_core_news_sm
+python3 -m spacy download el
 python3 -m spacy download xx_ent_wiki_sm
+python3 -m spacy download xx
 python3 -m spacy download en_core_web_lg
 python3 -m spacy download en_core_web_md
 
